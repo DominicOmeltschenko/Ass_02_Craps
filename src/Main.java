4ddaafc2ad0889c7be2733103a98dcde08bdfbe1
Dominic Omeltschenko <omeltsdf@mail.uc.edu>
2024-10-16 11:43:37 -0400

    code seems to be working, game function mostly as intended

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,11 @@ public class Main {
                 String playAgain = "";
                 String trash = "";
                 boolean done = false;
+                boolean stillRolling = false;
                 do
                 {
+
+
 
                         dieOne = new Random().nextInt(6) + 1;
                         dieTwo = new Random().nextInt(6) + 1;
@@ -23,19 +26,59 @@ public class Main {
                         {
                             System.out.println("You rolled: " + dieOne + " and " + dieTwo + " With a sum of: " + crapsRoll);
                             System.out.println("You crapped out! You lose.");
+                            stillRolling = false;
                         }
                         else if (crapsRoll == 7 || crapsRoll == 11)
                         {
                             System.out.println("You rolled: " + dieOne + " and " + dieTwo + " With a sum of: " + crapsRoll);
                             System.out.println("You got a natural! You win.");
+                            stillRolling = false;
                         }
                         else
                         {
                             pointNumber = crapsRoll;
                             System.out.println("You rolled: " + dieOne + " and " + dieTwo + " The Point is now: " + pointNumber);
+                            System.out.print("Roll again (hit enter to continue)");
+                            playAgain = in.nextLine();
+                            if (playAgain.equalsIgnoreCase("" ))
+                            {
+                                ;
+                                do {
+                                dieOne = new Random().nextInt(6) + 1;
+                                dieTwo = new Random().nextInt(6) + 1;
+                                crapsRoll = dieOne + dieTwo;
+
+                                if (crapsRoll == 7)
+                                {
+                                    System.out.println("You rolled a 7. You Lose!");
+                                    stillRolling = false;
+                                }
+                                else if (crapsRoll == pointNumber)
+                                {
+                                    System.out.println("You rolled the point. You Win!");
+                                    stillRolling = false;
+                                }
+                                else
+                                {
+                                    stillRolling = true;
+                                    System.out.println("You rolled: " + dieOne + " and " + dieTwo + " With a sum of: " + crapsRoll);
+                                    System.out.print("You didn't roll the point. Roll again (hit enter to continue)");
+                                    playAgain = in.nextLine();
+                                }
+
+                                }while (stillRolling);
+                            }
+                            else
+                            {
+                                trash = playAgain;
+                                System.out.println("You said: " + trash);
+                                System.out.print("Please say Y or N: ");
+                                playAgain = in.nextLine();
+                            }
                         }
 
-                    System.out.print("Would you like to roll again? (Y/N): ");
+
+                    System.out.print("Would you like to play again? (Y/N): ");
                     playAgain = in.nextLine();
                     if (playAgain.equalsIgnoreCase("N"))
                     {
